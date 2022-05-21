9bbdcb05d579aa866ca0d326b2898f79bbbab7c2
Rohit <rohitp934@gmail.com>
2022-05-21 13:55:23 +0530

    Added new functionality:
    - Added Play/Pause full functionality
    - Added extra button svgs
    - Added background gradient
    - Added CSS animations

--- projects/ngx-youtube-player/src/lib/ngx-youtube-player.component.ts
+++ projects/ngx-youtube-player/src/lib/ngx-youtube-player.component.ts
@@ -1,15 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 @Component({
   selector: 'ngx-youtube-player',
   templateUrl: `./ngx-youtube-player.component.html`,
-  styleUrls: ['./ngx-youtube-player.component.css']
+  styleUrls: ['./ngx-youtube-player.component.css'],
 })
 export class NgxYoutubePlayerComponent implements OnInit {
-  @Input() src: string = "";
+  @Input() src: string = '';
+  // Handle KeyDown Events
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    switch (event.key.toLowerCase()) {
+      case ' ':
+      case 'k':
+        this.togglePlay();
+        break;
+    }
+  }
+  // get child element .play-pause-btn with ViewChild
+  @ViewChild('playPauseBtn') playPauseBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
+  @ViewChild('videoContainer') videoContainer!: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // Play/Pause video
+  togglePlay() {
+    this.video.nativeElement.paused
+      ? this.video.nativeElement.play()
+      : this.video.nativeElement.pause();
   }
 
+  playVideo() {
+    this.videoContainer.nativeElement.classList.remove('paused');
+  }
+
+  pauseVideo() {
+    this.videoContainer.nativeElement.classList.add('paused');
+  }
 }
